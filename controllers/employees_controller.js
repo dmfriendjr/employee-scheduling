f3f522ff8d100e323026c8b7bc5d5292782d3a9b
dmfriendjr <davidmfriendjr@gmail.com>
2018-02-20 19:24:25 -0500

    Created relationship between users and employees in database with foreign key

--- controllers/employees_controller.js
+++ controllers/employees_controller.js
@@ -2,11 +2,26 @@ const express = require('express');
 const db = require('../models');
 
 const router = express.Router();
+db.users.hasMany(db.employees, {foreignKey: 'employer_id', constraints: false, as: 'employees'});
+db.employees.belongsTo(db.users, {foreignKey: 'employer_id', constraints: false, as: 'employer'});
 
-db.employees.belongsTo(db.users);
-
-router.get('/api/employees', function(req, res) {
+router.get('/api/employees', isLoggedIn, function(req, res) {
 
 });
+
+router.post('/api/employees/:uid', function(req, res) {
+  
+});
+
+// route middleware to make sure
+function isLoggedIn(req, res, next) {
+
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+}
 
 module.exports = router;
