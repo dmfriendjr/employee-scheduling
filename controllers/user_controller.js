9dc44b7779470589e28725e326bfd839c89bda1b
dmfriendjr <davidmfriendjr@gmail.com>
2018-02-22 14:50:11 -0500

    Added a catch if an invalid verification token or username was submitted

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -5,9 +5,14 @@ const router = express.Router();
 
 router.get('/verify/:username/:token', (req, res) => {
   db.users.findOne({where: {username: req.params.username, verificationToken: req.params.token}}).then( data => {
-    data.updateAttributes({verified: true});
-    req.flash('loginMessage', 'Email verified. Please login.');
-    res.redirect('/login');
+    if (data) {
+      data.updateAttributes({verified: true});
+      req.flash('loginMessage', 'Email verified. Please login.');
+      res.redirect('/login');
+    } else {
+      req.flash('loginMessage', 'Invalid verification token. Please contact support.');
+      res.redirect('/login');
+    }
   });
 });
 
