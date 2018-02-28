894cd6c90f93573eb7979353b7d8d7e145ffcfa6
dmfriendjr <davidmfriendjr@gmail.com>
2018-02-27 20:00:27 -0500

    Changed routes to redirect to home with appropriate flag variables

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -41,7 +41,7 @@ router.post('/resetRequest', (req, res) => {
       req.flash('resetMessage', 'Username does not match our records');
     }
 
-    res.redirect('/resetRequest');
+    res.redirect('/');
   });
 });
 
